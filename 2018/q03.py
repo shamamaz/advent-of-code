5a77fb7c0024e6fdbe0fe9af630538c112eb5436
Shamama Zehra <shamama.zehra@mail.utoronto.ca>
2018-12-10 23:18:52 -0500

    day 3 + day 4 fixes

---
+++ 2018/q03.py
@@ -0,0 +1,33 @@
+from aocd import data
+
+def helper(data):
+  multiple = 0
+  multiple_claim_ids = set()
+  all_ids = set()
+  grid = {}
+  for line in data.split('\n'):
+    claim_id, at, claim, dim = line.split(' ')
+    all_ids.add(claim_id)
+    x, y = map(int, claim.rstrip(':').split(','))
+    w, h = map(int, dim.split('x'))
+    for i in range(x, x + w):
+      for j in range(y, y + h):
+        pt = (i,j)
+        if pt in grid:
+          grid[pt].append(claim_id)
+          if len(grid[pt]) == 2:
+            multiple +=1
+          multiple_claim_ids.update(grid[pt])
+        else:
+          grid[pt] = [claim_id]
+
+  return multiple, (all_ids-multiple_claim_ids)
+
+def a(data):
+  return helper(data)[0]
+
+def b(data):
+  return helper(data)[1]
+
+print(a(data))
+print(b(data))
